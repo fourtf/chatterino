a3b291aeb298fefb3ea3d614be0ac686ce682463
fourtf <tf.four@gmail.com>
2016-08-16 20:19:21 +0200

    Added Eto.Forms basic template

---
+++ Chatterino.Eto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace Chatterino.Eto
+{
+	public static class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			// run application with our main form
+			new Application().Run(new MainForm());
+		}
+	}
+}
